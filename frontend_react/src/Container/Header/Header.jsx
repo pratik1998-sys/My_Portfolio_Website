8271c4945f82dc9f607a9997c3a00c19471f7fc3
pratik <pratiksingh260@gmail.com>
2023-12-27 17:50:15 +0530

    change heading

--- frontend_react/src/Container/Header/Header.jsx
+++ frontend_react/src/Container/Header/Header.jsx
@@ -32,8 +32,8 @@ const Header = () => {
             </div>
           </div>
           <div className='tag-cmp app__flex'>
+            <p className='p-text'>Frontend Developer</p>
             <p className='p-text'>Web Developer</p>
-            <p className='p-text'>Freelancer </p>
           </div>
         </div>
       </motion.div>
