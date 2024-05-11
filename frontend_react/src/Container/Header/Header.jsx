4a321213b596addf6c653c0d220d7b9129614ca6
pratik <pratiksingh260@gmail.com>
2024-05-11 23:48:08 +0530

    sass to mui logo change

--- frontend_react/src/Container/Header/Header.jsx
+++ frontend_react/src/Container/Header/Header.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import './header.scss'
-import { motion } from 'framer-motion'
-import { images } from '../../contants'
-import { AppWrap } from '../../wrapper'
+import React from "react";
+import "./header.scss";
+import { motion } from "framer-motion";
+import { images } from "../../contants";
+import { AppWrap } from "../../wrapper";
 
 const scaleVariants = {
   whileInView: {
@@ -10,39 +10,39 @@ const scaleVariants = {
     opacity: [0, 1],
     transition: {
       duaration: 1,
-      ease: 'easeInOut',
+      ease: "easeInOut",
     },
   },
-}
+};
 
 const Header = () => {
   return (
-    <div className='app__header app__flex'>
+    <div className="app__header app__flex">
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duaration: 0.5 }}
-        className='app__header-info'
+        className="app__header-info"
       >
-        <div className='app__header-badge'>
-          <div className='badge-cmp app__flex'>
+        <div className="app__header-badge">
+          <div className="badge-cmp app__flex">
             <span>👋</span>
             <div style={{ marginLeft: 10 }}>
-              <p className='p-text'>Hello, I am</p>
-              <h1 className='head-text'>Pratik</h1>
+              <p className="p-text">Hello, I am</p>
+              <h1 className="head-text">Pratik</h1>
             </div>
           </div>
-          <div className='tag-cmp app__flex'>
-            <p className='p-text'>Frontend Developer</p>
-            <p className='p-text'>Web Developer</p>
+          <div className="tag-cmp app__flex">
+            <p className="p-text">Frontend Developer</p>
+            <p className="p-text">Web Developer</p>
           </div>
         </div>
       </motion.div>
       <motion.div
         whileInView={{ opacity: [0, 1] }}
         transition={{ duaration: 0.5, delayChildren: 0.5 }}
-        className='app__header-img'
+        className="app__header-img"
       >
-        <img src={images.profile} alt='profile' className='profile-img' />
+        <img src={images.profile} alt="profile" className="profile-img" />
         {/* <motion.img
           whileInView={{ scale: [0, 1] }}
           transition={{ duaration: 1, ease: 'easeInOut' }}
@@ -54,16 +54,16 @@ const Header = () => {
       <motion.div
         variant={scaleVariants}
         whileInView={scaleVariants.whileInView}
-        className='app__header-circle'
+        className="app__header-circle"
       >
-        {[images.react, images.redux, images.sass].map((circle, index) => (
-          <div className='circle_cmp app__flex' key={`circle-${index}`}>
-            <img src={circle} alt='circle' />
+        {[images.react, images.redux, images.mu5].map((circle, index) => (
+          <div className="circle_cmp app__flex" key={`circle-${index}`}>
+            <img src={circle} alt="circle" />
           </div>
         ))}
       </motion.div>
     </div>
-  )
-}
+  );
+};
 
-export default AppWrap(Header, 'home')
+export default AppWrap(Header, "home");
